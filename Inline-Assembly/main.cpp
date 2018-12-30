66c524fa54d291827b36e7aa3efb7ba226036806
Martina Kraus <martinakraus@Martinas-MBP.fritz.box>
2018-12-30 21:14:48 +0100

    added inline-assembly example

--- Inline-Assembly/main.cpp
+++ Inline-Assembly/main.cpp
@@ -1,12 +1,15 @@
-#include <iostream>
 
-int main()
+
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
+int main(int argc, char *argv[])
 {
-    char *msg = "Hello Inline Assembly";
-
-    __asm__(
-        "mov rax,17"
-        "push rax;"
-        "call print_long;"
-        "pop rax");
+    int x = 1, f = 2, fa = 3;
+    EM_ASM("int $0x3");
+    EM_ASM("mov 4%0,%%eax" ::"m"(x));
+    EM_ASM("movss 4%0,%%xmm1" ::"m"(f));
+    EM_ASM("fldl 4%0" ::"m"(fa));
+    return 0;
 }
